958b4488737386f52edb7ca623b10ee49212b0ac
LoginmyWorld <98610167+Nagesh1001@users.noreply.github.com>
2022-07-02 16:45:37 +0530

    first commit

---
+++ src/Data.jsx
@@ -0,0 +1,46 @@
+const DATA = [
+    {
+        id : 0,
+        title : " Smitch Static WiFi Camera",
+        price : 1499,
+        desc : "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        img : "/assets/images/products/Smitch-Static-WiFi-Camera.webp"
+    },
+    {
+        id : 1,
+        title : "Smitch Wi-Fi White Ambience",
+        price : 499,
+        desc : "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        img : "/assets/images/products/Smitch Wi-Fi White Ambience.webp"
+    },
+    {
+        id : 2,
+        title : "Smitch Wi-Fi Smart Security Camera",
+        price : 2299,
+        desc : "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        img : "/assets/images/products/Smitch Wi-Fi Smart Security Camera.webp"
+    },
+    {
+        id : 3,
+        title : "Smitch WI-FI RGB - (7W)",
+        price : 699,
+        desc : "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        img : "/assets/images/products/Smitch WI-FI RGB - (7W).webp"
+    },
+    {
+        id : 4,
+        title : "Smart Wi-Fi Night Lamp",
+        price : 999,
+        desc : "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        img : "/assets/images/products/night-lamp-cnw.png"
+    },
+    {
+        id : 5,
+        title : "Smitch Wi-Fi White Ambience Bulb (10W)",
+        price : 499,
+        desc : "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        img : "/assets/images/products/10-W-white-dimmable-bulb.png"
+    }
+]
+
+export default DATA;
